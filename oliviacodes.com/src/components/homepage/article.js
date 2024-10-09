cb3f36edaa2145136033306e5181f4af2898d321
OliviaTetteh <olivet36@yahoo.com>
2024-10-09 04:47:45 +0000

    js file for article

---
+++ oliviacodes.com/src/components/homepage/article.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Article from "./Article";
+
+const ArticleList = () => {
+	return (
+		<div>
+			<Article
+				title="My Portfolio Journey"
+				description="This article outlines my journey to building my portfolio project."
+				date="October 9, 2024"
+				link="https://www.linkedin.com/article/edit/7247956308547493888"
+
+			/>
+		</div>
+	);
+};
+
+export default SingleArticle;
+
