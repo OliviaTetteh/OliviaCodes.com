5d790ba2e8ac0cabd9b8a49f90c03248ffecf9ad
OliviaTetteh <olivet36@yahoo.com>
2024-10-11 20:12:11 +0000

    react

--- oliviacodes.com/src/App.js
+++ oliviacodes.com/src/App.js
@@ -1,27 +1,38 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import NavBar from './components/common/navBar';
-import Home from './pages/homepage';
-import Projects from './pages/projects';
-import ReadArticle from './pages/readArticle';
-import Footer from './components/common/footer';
-import './styles/App.css';
+import { useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import ReactGA from "react-ga4";
 
-const App = () => {
-  return (
-    <Router>
-      <div className="App">
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/articles/:slug" element={<ReadArticle />} />
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
-  );
-};
+import Homepage from "./pages/homepage";
+import About from "./pages/about";
+import Projects from "./pages/projects";
+import Articles from "./pages/articles";
+import ReadArticle from "./pages/readArticle";
+import Contact from "./pages/contact";
+import Notfound from "./pages/404";
+
+import { TRACKING_ID } from "./data/tracking";
+import "./app.css";
+
+function App() {
+	useEffect(() => {
+		if (TRACKING_ID !== "") {
+			ReactGA.initialize(TRACKING_ID);
+		}
+	}, []);
+
+	return (
+		<div className="App">
+			<Routes>
+				<Route path="/" element={<Homepage />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/projects" element={<Projects />} />
+				<Route path="/articles" element={<Articles />} />
+				<Route path="/article/:slug" element={<ReadArticle />} />
+				<Route path="/contact" element={<Contact />} />
+				<Route path="*" element={<Notfound />} />
+			</Routes>
+		</div>
+	);
+}
 
 export default App;
-
