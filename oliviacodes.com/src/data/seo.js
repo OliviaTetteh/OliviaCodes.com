d29b700f172a496d1beceb8275369c40471a0de9
OliviaTetteh <olivet36@yahoo.com>
2024-10-11 04:52:41 +0000

    seo updated

---
+++ oliviacodes.com/src/data/seo.js
@@ -0,0 +1,38 @@
+const SEO = [
+	{
+		page: "home",
+		description:
+			"I am a web developer specialized in frontend development with expertise in JavaScript, React, HTML, and CSS. I have experience in building scalable, secure, and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am eager to find a creative and challenging role that will allow me to further hone my skills and contribute meaningfully to innovative projects",
+		keywords: ["Olivia Tetteh", "Olivia Ahinee Tetteh"],
+	},
+
+	{
+		page: "about",
+		description:
+			"I’ve had the opportunity to work on a diverse range of projects over the years, and I’m proud of the progress I've achieved. If you're curious about any of my work, feel free to explore the code and share any suggestions for improvements or enhancements. Collaboration is a valuable way to learn and grow, and I’m always open to fresh ideas and constructive feedback.",
+		keywords: ["Olivia Tetteh", "Olivia Ahinee Tetteh"],
+	},
+
+	{
+		page: "articles",
+		description:
+			"I'm dedicated to documenting my journey in everything I do, so I can look back and appreciate the effort I've put in while also inspiring others who come across my work.",
+		keywords: ["Olivia Tetteh", "Olivia Ahinee Tetteh"],
+	},
+
+	{
+		page: "projects",
+		description:
+			"I’ve had the opportunity to work on a diverse range of projects over the years, and I’m proud of the progress I've achieved. If you're curious about any of my work, feel free to explore the code and share any suggestions for improvements or enhancements. Collaboration is a valuable way to learn and grow, and I’m always open to fresh ideas and constructive feedback.",
+		keywords: ["Olivia Tetteh", "Olivia Ahinee Tetteh"],
+	},
+
+	{
+		page: "contact",
+		description:
+			"If you're interested in collaborating on a project, feel free to reach out to me. I'm always open to new ideas and opportunities.",
+	        keywords: ["Olivia Tetteh", "Olivia Ahinee Tetteh"],
+	},
+];
+
+export default SEO;
