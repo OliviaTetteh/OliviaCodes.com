6912ceca1c750d23be404625a778bc35c485a918
OliviaTetteh <olivet36@yahoo.com>
2024-10-11 04:21:25 +0000

    pages titles

---
+++ oliviacodes.com/src/data/user.js
@@ -0,0 +1,57 @@
+const INFO = {
+	main: {
+		title: "OliviaCodes",
+		name: "Olivia Tetteh",
+		email: "oatetteh@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		twitter: "https://twitter.com/OliviaTetteh5",
+		github: "https://github.com/OliviaTetteh",
+		linkedin: "https://linkedin.com/in/olivia-ahinee-tetteh-0257468a/",
+	},
+
+	homepage: {
+		title: "Full-stack web developer",
+		description:
+			"I am a web developer specialized in frontend developement with expertise in JavaScript, React, HTML, CSS. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am eager to find a creative and challenging role that will allow me to further hone my skills and contribute meaningfully to innovative projects.",
+	},
+
+	about: {
+		title: "Hello World! I'm Olivia Ahinee Tetteh, a creative from Ghana, envisioning and crafting the designs of the future.",
+		description:
+			"I’ve had the opportunity to work on a diverse range of projects over the years, and I’m proud of the progress I've achieved. If you're curious about any of my work, feel free to explore the code and share any suggestions for improvements or enhancements. Collaboration is a valuable way to learn and grow, and I’m always open to fresh ideas and constructive feedback.",
+	},
+
+	articles: {
+		title: "I'm dedicated to documenting my journey in everything I do, so I can look back and appreciate the effort I've put in while also inspiring others who come across my work.",
+		description:
+			"I'm dedicated to documenting my journey in everything I do, so I can look back and appreciate the effort I've put in while also inspiring others who come across my work.",
+	about: {
+		"A curated timeline of my in-depth reflections on my journey as a developer, programming, leadership, product design, and beyond.",
+	},
+
+	projects: [
+		{
+			title: "Olive Blog App",
+			description:
+				"This project is a simple blogging platform where users can create accounts, write blog posts and comment on others posts. It was built with Flask, HTML, Bootstrap and an SQLite Database.",
+			logo: "../Olive Blog.png",
+			linkText: "View Project",
+			link: "https://github.com/OliviaTetteh/Olive-Blog-App",
+		},
+		
+		{
+			title: "Adziban"
+			description:
+				"This is a website where users can discover and share african recipes, with the ability to search, save and rate them. It features user registration and login, search functionality by ingredients or recipe name, recipe submission and categorization, rating and commenting on recipes, favourite list for users",
+			logo: "../Adziban.png",
+			linkText: "View Project",
+			link: "https://github.com/OliviaTetteh/Adziban",
+
+	        }
+	],
+};
+
+export default INFO;
