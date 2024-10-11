65ecabbedcb40192fc9d6b6965eaf6e707651694
OliviaTetteh <olivet36@yahoo.com>
2024-10-11 04:58:26 +0000

    seo updated

--- oliviacodes.com/src/data/seo.js
+++ oliviacodes.com/src/data/seo.js
@@ -2,7 +2,7 @@ const SEO = [
 	{
 		page: "home",
 		description:
-			"I am a web developer specialized in frontend development with expertise in JavaScript, React, HTML, and CSS. I have experience in building scalable, secure, and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am eager to find a creative and challenging role that will allow me to further hone my skills and contribute meaningfully to innovative projects",
+			"I am a web developer specialized in frontend development with expertise in JavaScript, React, HTML, and CSS. I have experience in building scalable, secure, and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am eager to find a creative and challenging role that will allow me to further hone my skills and contribute meaningfully to innovative projects.",
 		keywords: ["Olivia Tetteh", "Olivia Ahinee Tetteh"],
 	},
 
@@ -31,8 +31,9 @@ const SEO = [
 		page: "contact",
 		description:
 			"If you're interested in collaborating on a project, feel free to reach out to me. I'm always open to new ideas and opportunities.",
-	        keywords: ["Olivia Tetteh", "Olivia Ahinee Tetteh"],
+		keywords: ["Olivia Tetteh", "Olivia Ahinee Tetteh"],
 	},
 ];
 
 export default SEO;
+
