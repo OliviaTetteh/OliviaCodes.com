771300178fc0ef8cea9db6f3bb0cd33367f4ef39
OliviaTetteh <olivet36@yahoo.com>
2024-10-11 19:51:17 +0000

    react

--- oliviacodes.com/src/App.js
+++ oliviacodes.com/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavBar from './components/common/navBar';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import ReadArticle from './pages/ReadArticle';
+import Footer from './components/common/footer';
+import './styles/App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/articles/:slug" element={<ReadArticle />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
+
