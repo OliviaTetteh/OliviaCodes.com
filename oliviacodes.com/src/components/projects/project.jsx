10d613f57be97d87a4ee801cc2db7bff271f6db2
OliviaTetteh <olivet36@yahoo.com>
2024-10-13 06:22:16 +0000

    update on imported image

--- oliviacodes.com/src/components/projects/project.jsx
+++ oliviacodes.com/src/components/projects/project.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Project from "./Project";
+import Project from "./project";
 import OliveBlogLogo from "../public/Olive Blog.png";
 import AdzibanLogo from "../public/Adziban.png";
 
