f90c24caafdd190d748e2d8f434775e929891c06
OliviaTetteh <olivet36@yahoo.com>
2024-10-14 02:39:49 +0000

    updates to project.jsx file

--- oliviacodes.com/src/components/projects/project.jsx
+++ oliviacodes.com/src/components/projects/project.jsx
@@ -10,14 +10,14 @@ const ProjectsList = () => {
 	return (
 		<div className="projects-list">
 			<Project
-				logo={OliveBlogLogo}
+				logo={OliveBlog}
 				title="Olive Blog App"
 				description="A simple blogging platform where users can create accounts, write blog posts, and comment on others' posts."
 				linkText="View Here"
 				link="https://github.com/OliviaTetteh/Olive-Blog-App"
 			/>
 			<Project
-				logo={AdzibanLogo}
+				logo={Adziban}
 				title="Adziban"
 				description="A website where users can discover and share African recipes, with the ability to search, save, and rate them."
 				linkText="View Here"
