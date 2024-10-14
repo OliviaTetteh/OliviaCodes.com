bc2a70bf6649c769ef0f950098805e2ff1ff1f5c
OliviaTetteh <olivet36@yahoo.com>
2024-10-14 05:02:10 +0000

    index.js

--- oliviacodes.com/src/index.js
+++ oliviacodes.com/src/index.js
@@ -8,7 +8,9 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
@@ -16,3 +18,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
